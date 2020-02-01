29e00bb1d6833d83db45784e223b7e00ec19fa7b
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-01 12:28:10 +0530

    Added handlers
    What changes you have made?
    1. Added handlers
        * serveNotFoundPage
        * serveBadRequestPage
        * serveStaticFile
        * readBody

--- handlers.js
+++ handlers.js
@@ -1,2 +1,71 @@
 'use strict';
 
+const fs = require('fs');
+const queryString = require('querystring');
+
+const CONTENT_TYPES = require('./lib/mimeTypes');
+const App = require('./app');
+
+const STATIC_FOLDER = `${__dirname}/public`;
+
+const areStatsOk = path => {
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  return stat && stat.isFile();
+};
+
+const decideUrl = url => {
+  return url === '/' ? '/index.html' : url;
+};
+
+const serveStaticFile = (req, res, next) => {
+  const url = decideUrl(req.url);
+  const path = `${STATIC_FOLDER}${url}`;
+  if (!areStatsOk(path)) {
+    return next();
+  }
+  const [, extension] = path.match(/.*\.(.*)$/) || [];
+  const content = fs.readFileSync(path);
+  res.setHeader('Content-Type', CONTENT_TYPES[extension]);
+  res.setHeader('Content-Length', content.length);
+  res.end(content);
+};
+
+const readBody = (req, res, next) => {
+  let data = '';
+  req.on('data', chunk => {
+    data += chunk;
+  });
+  req.on('end', () => {
+    req.body = queryString.parse(data);
+    next();
+  });
+};
+
+const serveNotFoundPage = (req, res) => {
+  const content = '404 File Not Found';
+  res.setHeader('Content-Type', CONTENT_TYPES.txt);
+  res.setHeader('Content-Length', content.length);
+  res.statusCode = 404;
+  res.end(content);
+};
+
+const serveBadRequestPage = (req, res) => {
+  const content = '400 Method Not Allowed';
+  res.setHeader('Content-Type', CONTENT_TYPES.txt);
+  res.setHeader('Content-Length', content.length);
+  res.statusCode = 400;
+  res.end(content);
+};
+
+const app = new App();
+
+app.get('', serveStaticFile);
+app.get('', serveNotFoundPage);
+
+app.use(readBody);
+
+app.post('', serveNotFoundPage);
+
+app.use(serveBadRequestPage);
+
+module.exports = app;
