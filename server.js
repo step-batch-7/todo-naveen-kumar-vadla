a42241019e853f6e8829e657bf1446b0c6717ff2
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-02-10 11:49:58 +0530

    moved setUpDataBase from fsUtils to server.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const { Server } = require('http');
+const fs = require('fs');
+
 const app = require('./lib/handlers');
 
 const defaultPort = 7000;
 
+const setUpDataBase = function() {
+  const DATA_PATH = `${__dirname}/../data`;
+  if (!fs.existsSync(DATA_PATH)) {
+    fs.mkdirSync(DATA_PATH);
+  }
+};
+
 const main = (port = defaultPort) => {
+  setUpDataBase();
   const server = new Server(app.handleRequests.bind(app));
   server.listen(port, () => process.stderr.write(`started listening: ${port}`));
 };
